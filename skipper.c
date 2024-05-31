fd047a48d991082ce0728d32f4087bc1827d7079
Alp Helvaci <git@alphelvaci.com>
2024-05-31 21:09:16 +0300

    Initial commit

---
+++ skipper.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define COLS 35
+#define ROWS COLS
+
+#include "skipper.h"
+
+double skip_per(crs_t crs, ccs_t ccs) {
+    return 0.0;
+}
+
+int main() {
+    FILE* file = fopen("ey35_02.mat", "r");
+
+    if (file == NULL) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
+
+    int cols, non_zeros;
+    if (fscanf(file, "%d %d", &cols, &non_zeros) != 2 || cols != COLS) { // NOLINT(*-err34-c)
+        perror("Invalid file");
+        return EXIT_FAILURE;
+    }
+
+    double matrix[ROWS][COLS] = {0.0};
+    for (int i=0; i < non_zeros; i++) {
+        int row, col;
+        double value;
+        if (fscanf(file, "%d %d %lf", &row, &col, &value) != 3) { // NOLINT(*-err34-c)
+            perror("Invalid file");
+            return EXIT_FAILURE;
+        }
+        matrix[row][col] = value;
+    }
+
+    fclose(file);
+
+    int rows = cols;
+    printf("Matrix read. Size %dx%d with %d non-zeros.\n", rows, cols, non_zeros);
+
+    crs_t matrix_crs = {
+            malloc((rows+ 1) * sizeof(int)),
+            malloc(non_zeros * sizeof(int)),
+            malloc(non_zeros * sizeof(double))
+    };
+    matrix_crs.row_pointers[0] = 0;
+    for (int i=0; i < rows; i++) {
+        matrix_crs.row_pointers[i+1] = matrix_crs.row_pointers[i];
+        for (int j = 0; j < cols; j++) {
+            if (matrix[i][j] != 0) {
+                matrix_crs.columns[matrix_crs.row_pointers[i+1]] = j;
+                matrix_crs.row_values[matrix_crs.row_pointers[i+1]] = matrix[i][j];
+                matrix_crs.row_pointers[i+1] += 1;
+            }
+        }
+    }
+
+    ccs_t matrix_ccs = {
+            malloc((cols + 1) * sizeof(int)),
+            malloc(non_zeros * sizeof(int)),
+            malloc(non_zeros * sizeof(double))
+    };
+    matrix_ccs.column_pointers[0] = 0;
+    for (int j = 0; j < cols; j++) {
+        matrix_ccs.column_pointers[j+1] = matrix_ccs.column_pointers[j];
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][j] != 0) {
+                matrix_ccs.rows[matrix_ccs.column_pointers[j+1]] = i;
+                matrix_ccs.column_values[matrix_ccs.column_pointers[j+1]] = matrix[i][j];
+                matrix_ccs.column_pointers[j+1] += 1;
+            }
+        }
+    }
+
+    if (matrix_crs.row_pointers[rows] != non_zeros || matrix_ccs.column_pointers[cols] != non_zeros) {
+        perror("CRS/CCS error");
+
+        free(matrix_crs.row_pointers);
+        free(matrix_crs.columns);
+        free(matrix_crs.row_values);
+
+        free(matrix_ccs.column_pointers);
+        free(matrix_ccs.rows);
+        free(matrix_ccs.column_values);
+
+        return EXIT_FAILURE;
+    }
+
+    double permanent = skip_per(matrix_crs, matrix_ccs);
+    printf("The permanent is %lf.", permanent);
+
+    free(matrix_crs.row_pointers);
+    free(matrix_crs.columns);
+    free(matrix_crs.row_values);
+
+    free(matrix_ccs.column_pointers);
+    free(matrix_ccs.rows);
+    free(matrix_ccs.column_values);
+
+    return EXIT_SUCCESS;
+}
---
+++ skipper.h
@@ -0,0 +1,25 @@
+#ifndef COLS
+#define COLS 32
+#endif
+
+#ifndef ROWS
+#define ROWS COLS
+#endif
+
+#ifndef SKIPPER
+#define SKIPPER
+typedef struct crs_s {
+    int* row_pointers;
+    int* columns;
+    double* row_values;
+} crs_t;
+
+typedef struct ccs_s {
+    int* column_pointers;
+    int* rows;
+    double* column_values;
+} ccs_t;
+
+void skip_ord(int* matrix[ROWS][COLS]);
+double skip_per(crs_t crs, ccs_t ccs);
+#endif
